97e885eb258d426a065c81cce533d07cb85426ec
nikiyyy <44340403+nikiyyy@users.noreply.github.com>
2021-11-09 16:19:14 +0200

    Add files via upload

--- secretSanta/secretSanta_WriteToFile.py
+++ secretSanta/secretSanta_WriteToFile.py
@@ -1,6 +1,7 @@
 import os
 from random import shuffle
 
+# used for the input of names in the "participants" list
 personInput=None
 participants = []
 
@@ -11,16 +12,20 @@ while personInput != 0:
         break
     participants.append(personInput)
 
-
+#checks if the folder that contains the files exists, if not, it creates it
 if 'Secret santa folder' not in os.listdir():
     os.chdir(r'C:\Users\Niki\Documents\python\secretSanta\Secret santa folder')
     
+# shuffles the list of names
 shuffle(participants)
+
+#creates files, with the name of the participants, containing the name of the perseon that will receave a gift from the person
 for i in range(len(participants)):
     for j in range(i+1, len(participants)):
         with open(participants[i]+".txt", 'w') as writer:
             writer.write( participants[j])
         break
+    # for the last person in the list
     if i+1 == len(participants):
         with open(participants[i]+".txt", 'w') as writer:
             writer.write(participants[0])
--- secretSanta/secretSanta_consoleOutput.py
+++ secretSanta/secretSanta_consoleOutput.py
@@ -1,5 +1,6 @@
 from random import shuffle
 
+# used for the input of names in the "participants" list
 personInput=None
 participants = []
 
@@ -9,13 +10,17 @@ while personInput != 0:
     if personInput=="0":
         break
     participants.append(personInput)
-    
+   
+# shuffles the list of names   
 shuffle(participants)
+
+#creates files, with the name of the participants, containing the name of the perseon that will receave a gift from the person
 for i in range(len(participants)):
     for j in range(i+1, len(participants)):
         print("{} -> {}".format(participants[i], participants[j]))
-        
         break
+    
+    # for the last person in the list
     if i+1 == len(participants):
         print("{} -> {}".format(participants[len(participants)-1], participants[0]))
 
